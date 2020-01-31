ca9859edc3b6f94ea8cc604f8b1efc453e40f4bc
raizengfilosofuxd <rickdavidsangalang@gmail.com>
2020-01-31 21:41:53 +0800

    Initialize

---
+++ folder/cropper.js
@@ -0,0 +1,20 @@
+// import 'cropperjs/dist/cropper.css';
+<script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.6/cropper.min.js" integrity="sha256-EuV9YMxdV2Es4m9Q11L6t42ajVDj1x+6NZH4U1F+Jvw=" crossorigin="anonymous">
+
+</script>
+
+import Cropper from 'cropperjs';
+
+const image = document.getElementById('image');
+const cropper = new Cropper(image, {
+  aspectRatio: 16 / 9,
+  crop(event) {
+    console.log(event.detail.x);
+    console.log(event.detail.y);
+    console.log(event.detail.width);
+    console.log(event.detail.height);
+    console.log(event.detail.rotate);
+    console.log(event.detail.scaleX);
+    console.log(event.detail.scaleY);
+  },
+});
